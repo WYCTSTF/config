6451fbe3dc9183aa0452c0c95e0de2f68f655cf5
WYCTSTF <996098413@qq.com>
2023-05-30 20:12:52 +0800

    2023.5.30

--- config.fish
+++ config.fish
@@ -1,12 +1,27 @@
-set fish_greeting '都什么年代，还在用传统shell'
+function fish_prompt -d "Write out the prompt"
 
-function fish_title
-	echo "我测你码 | "(prompt_pwd)
+printf '╭─%s@%s | HUAWEI 
+╰─%s%s%s: ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+
 end
 
-alias py=python
+# function fish_title
+# 	echo "玩原神玩的 | "(prompt_pwd)
+# end
 
-export TERM="xterm-256color"
+alias py=python
+alias nvi=nvim
+alias lla='ll -a'
+alias lsa='ls -a'
+alias cdf='cd $(find * -type d | fzf)'
+alias gst='git status'
+alias gl='git pull'
+alias gp='git push'
+alias ga='git add'
+alias gcmsg='git commit -m'
+
+# export TERM="xterm-256color"
+export SHELL="fish"
 export EDITOR="nvim"
 export VCPKG_ROOT="/Users/syh/vcpkg"
 
@@ -20,38 +35,4 @@ set PATH /opt/homebrew/opt/gawk/libexec/gnubin        $PATH
 set PATH /usr/local/mysql/support-files               $PATH
 set PATH /Users/syh/.ghcup/bin                        $PATH
 set PATH /usr/local/texlive/2021/bin/universal-darwin $PATH
-set PATH /Users/syh/cnblog/node_modules/.bin			    $PATH
 set PATH /opt/homebrew/Cellar                         $PATH
-
-set NOCLOBBER 1
-
-# Proxy switcher
-function proxy
-  if test "$argv[1]" = "on"
-    if test "$argv[2]" = ""
-      echo "No port provided"
-      return 2
-    end
-    # proxy offered by local shadowsocks
-    export http_proxy="http://127.0.0.1:$argv[2]"
-    export https_proxy="http://127.0.0.1:$argv[2]"
-  else if test "$argv[1]" = "off"
-    set -e http_proxy
-    set -e https_proxy
-  else if test "$argv[1]" != ""
-    echo "Usage: 
-        proxy          - view current proxy
-        proxy on PORT  - turn on proxy at localhost:PORT
-        proxy off      - turn off proxy"
-    return 1
-  end
-  echo "Current: http_proxy=$http_proxy https_proxy=$https_proxy"
-end
-
-# conda configuration
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
