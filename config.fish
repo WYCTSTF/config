7ad2b2d6fcfc16949c58fb6ff1413e3f02e1f0fd
WYCTSTF <996098413@qq.com>
2023-06-13 11:09:19 +0800

    2023.6.13

--- config.fish
+++ config.fish
@@ -1,24 +1,21 @@
-function fish_prompt -d "Write out the prompt"
+function fish_prompt -d "Ah?"
 
 printf '╭─%s@%s | HUAWEI 
 ╰─%s%s%s: ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 
 end
 
-# function fish_title
-# 	echo "玩原神玩的 | "(prompt_pwd)
-# end
-
 alias py=python
 alias nvi=nvim
-alias lla='ll -a'
-alias lsa='ls -a'
+alias hexo="npx hexo"
+alias nfz='nvim $(fzf)'
 alias cdf='cd $(find * -type d | fzf)'
 alias gst='git status'
 alias gl='git pull'
 alias gp='git push'
 alias ga='git add'
 alias gcmsg='git commit -m'
+alias nproc="sysctl -n hw.logicalcpu"
 
 # export TERM="xterm-256color"
 export SHELL="fish"
@@ -29,6 +26,7 @@ export VCPKG_ROOT="/Users/syh/vcpkg"
 source ~/.config/fish/nvm-wrapper/nvm.fish
 
 # PATH settings
+set PATH /usr/local/bin                               $PATH
 set PATH /opt/homebrew/bin /opt/homebrew/opt/llvm/bin $PATH
 set PATH /opt/homebrew/sbin														$PATH
 set PATH /opt/homebrew/opt/gawk/libexec/gnubin        $PATH
@@ -36,3 +34,11 @@ set PATH /usr/local/mysql/support-files               $PATH
 set PATH /Users/syh/.ghcup/bin                        $PATH
 set PATH /usr/local/texlive/2021/bin/universal-darwin $PATH
 set PATH /opt/homebrew/Cellar                         $PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
