958c59ba502af3ecf94c3861d591b64ed42ec2ae
WYCTSTF <996098413@qq.com>
2024-10-11 14:48:06 +0800

    2024.10.11

--- config.fish
+++ config.fish
@@ -1,11 +1,13 @@
 function fish_prompt -d "Ah?"
 printf '╭─%s@%s
-╰─%s%s%s: ' 'wcb-dad ' " HUAWEI M1 Air " (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-# %s%s: ' 'wcb-dad' (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+╰─%s%s%s: ' 'wcb-dad ' " HUAWEI Matepad Air " (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  # %s%s: ' 'wcb-dad' (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
 
 alias cls='clear'
 alias vim=nvim
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
 alias avim='NVIM_APPNAME=AstroNvim nvim'
 alias lvim='NVIM_APPNAME=lazyVim nvim'
 alias kvim='NVIM_APPNAME=kickstartNvim nvim'
@@ -19,29 +21,25 @@ alias gcmsg='git commit -m'
 alias nproc="sysctl -n hw.logicalcpu"
 alias lg='lazygit'
 alias rm='trash'
+alias cf='cf-tool'
+alias leetcode='neovide leetcode.nvim'
 
 # export TERM="xterm-256color"
 export SHELL="fish"
 export EDITOR="nvim"
 export VCPKG_ROOT="/Users/syh/vcpkg"
 
-# nvm settings
-source ~/.config/fish/nvm-wrapper/nvm.fish
-
 # PATH settings
-set PATH /usr/local/bin                               $PATH
-set PATH /Users/syh/IDAPro/ida.app/Contents/MacOS/    $PATH
-set PATH /opt/homebrew/bin /opt/homebrew/opt/llvm/bin $PATH
-set PATH /opt/homebrew/sbin														$PATH
-set PATH /opt/homebrew/opt/gawk/libexec/gnubin        $PATH
-set PATH /usr/local/mysql/support-files               $PATH
-set PATH /Users/syh/.ghcup/bin                        $PATH
-set PATH /usr/local/texlive/2021/bin/universal-darwin $PATH
-set PATH /opt/homebrew/Cellar                         $PATH
+set PATH /home/syh/clang+llvm-18.1.8-aarch64-linux-gnu/bin  $PATH
+
+set -x PATH $PATH /usr/local/go/bin
+set -x GOPATH $HOME/go_projects
+set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH /usr/local/texlive/2024/bin/aarch64-linux
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+if test -f ~/miniconda3/bin/conda
+  eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
 # <<< conda initialize <<<
